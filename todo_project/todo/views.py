5de19af57591d1073e2bdcd1df5dd1d1b167307d
Muhammad Jafar <jafarshodiq0114@gmail.com>
2024-10-24 19:41:06 +0700

    add bootstrap

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from .models import TodoItem
 from .forms import TodoForm
 
+# view menu todo list
 def todo_list(request):
     todos = TodoItem.objects.all()
     return render(request, 'todo/todo_list.html', {'todos': todos})
 
+# view menu add to do
 def add_todo(request):
     if request.method == 'POST':
         form = TodoForm(request.POST)
@@ -16,11 +18,13 @@ def add_todo(request):
         form = TodoForm()
     return render(request, 'todo/add_todo.html', {'form': form})
 
+# function delete list
 def delete_todo(request, todo_id):
     todo = TodoItem.objects.get(id=todo_id)
     todo.delete()
     return redirect('todo_list')
 
+# function mark complete list
 def mark_as_completed(request, todo_id):
     todo = TodoItem.objects.get(id=todo_id)
     todo.completed = True
